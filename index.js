27960faec773760d3448916fd11349dc6ef80933
Eduardo Lima <edulima@gmail.com>
2018-04-08 23:19:22 -0300

    Fazendo WETH ser o maker e o ZRX o taker

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ exports.__esModule = true;
 var _0xorderbuilder_1 = require("0xorderbuilder");
 var bignumber_js_1 = require("bignumber.js");
 function testZeroExOrderBuilder() {
-    _0xorderbuilder_1.ZeroExOrderBuilder.buildQuoteProviderOrder("0xf60345bcff9feedb98bbdfc996b33cba00ee2c75", "0x6ff6c0ff1d68b964901f986d4c9fa3ac68346570", "0xd0a1e359811322d97991e03f863a0c30c2cf029c", new bignumber_js_1.BigNumber(1000000000000000000), new bignumber_js_1.BigNumber(10000), "http://localhost:3000/api/v0", _0xorderbuilder_1.EthNetwork.Kovan).then(function (order) {
-        var signedOrder = _0xorderbuilder_1.ZeroExOrderBuilder.buildSignedOrder(order, "0x5edd9d13a5d62821bbea8ac6da7d7ca69a1b540dc99ac9232fefc04d09e28055");
+    _0xorderbuilder_1.ZeroExOrderBuilder.buildQuoteProviderOrder("0xb39a559ee400ed11a985d18168f2a8f10fc901f3", "0xd0a1e359811322d97991e03f863a0c30c2cf029c", "0x6ff6c0ff1d68b964901f986d4c9fa3ac68346570", new bignumber_js_1.BigNumber(100000000000000), new bignumber_js_1.BigNumber(100000000000), "http://localhost:3000/api/v0", _0xorderbuilder_1.EthNetwork.Kovan).then(function (order) {
+        var signedOrder = _0xorderbuilder_1.ZeroExOrderBuilder.buildSignedOrder(order, "0x6e52331cc0a81c73014d2289a4676952ff2364d85c1c63c126753018fd49b7e6");
         console.log(JSON.stringify(signedOrder));
     });
 }
+"661728405000000000000";
 testZeroExOrderBuilder();
--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import { ZeroExOrderBuilder, EthNetwork } from "0xorderbuilder";
 import { BigNumber } from "bignumber.js";
 
 function testZeroExOrderBuilder() {
-	ZeroExOrderBuilder.buildQuoteProviderOrder("0xf60345bcff9feedb98bbdfc996b33cba00ee2c75", "0x6ff6c0ff1d68b964901f986d4c9fa3ac68346570",
-		"0xd0a1e359811322d97991e03f863a0c30c2cf029c", new BigNumber(1000000000000000000), new BigNumber(10000),
+	ZeroExOrderBuilder.buildQuoteProviderOrder("0xf60345bcff9feedb98bbdfc996b33cba00ee2c75", "0xd0a1e359811322d97991e03f863a0c30c2cf029c", 
+		"0x6ff6c0ff1d68b964901f986d4c9fa3ac68346570", new BigNumber(100000000000000), new BigNumber(100000),
 		"http://localhost:3000/api/v0", EthNetwork.Kovan).then((order: any) => {
 			const signedOrder = ZeroExOrderBuilder.buildSignedOrder(order, "0x5edd9d13a5d62821bbda8ac6da7d7ca69a1b540dc99ac9232fefc04d09e28055");
 			console.log(JSON.stringify(signedOrder));
